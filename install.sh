6a73c3845eba39f06df38552dd1b40b7026a006f
Ahmy <ahmy@sense-os.nl>
2013-05-01 09:58:56 +0100

    add pystartup to zsh

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -s ~/.configurations/vim/dotvim ~/.vim
 ln -s ~/.configurations/vim/dotvimrc ~/.vimrc
 ln -s ~/.configurations/tmux/dottmux.conf ~/.tmux.conf
 ln -s ~/.configurations/zsh/dotoh-my-zsh ~/.oh-my-zsh
+ln -s ~/.configurations/python/pystartup ~/.pystartup
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
